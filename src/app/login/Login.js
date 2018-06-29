4c983116a7dc596c1bafd565aea5f8b57c16682e
Mateus <mateus@ornito.com.br>
2018-06-29 15:52:00 -0300

    Create Layout login

--- src/app/login/Login.js
+++ src/app/login/Login.js
@@ -1,9 +1,42 @@
 import React from "react";
-
+import './Login.css'
+import logo from '../../squaddash-logo.png' ;
 export class Login extends React.Component {
   render() {
     return (
-      <h1>Login</h1>
+      <section className='section'>
+        <div className='container'>
+          <img src={logo} className='login-logo'/>
+          <div className="tile is-parent is-vertical login-box">
+            <article className="tile is-child">
+              <p className="subtitle has-text-centered">Login</p>
+              <div className="field">
+                <label className="label">Email</label>
+                <div className="control">
+                  <input className="input" type="email" placeholder="exemplo@dominio.com.br"/>
+                </div>
+              </div>
+              <div className="field">
+                <label className="label">Senha</label>
+                <div className="control">
+                  <input className="input" type="password" placeholder="mínimo 6 caractéres" minLength='6'/>
+                </div>
+              </div>
+              <div className="columns">
+                <div className='column is-9'>
+                  <a className='links'> Cadastrar-se </a>
+                </div>
+                <div className='column is-3 esqueceu-a-senha'>
+                  <a className='links'> Esqueceu a senha </a>
+                </div>
+              </div>
+              <div className="control">
+                <button className="button is-info btn-full">Entrar</button>
+              </div>
+            </article>
+          </div>
+        </div>
+      </section>
     );
   }
 }
