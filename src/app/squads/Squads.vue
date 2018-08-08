cf49361fe5bf2de5011c2a667e833d3dab642e1f
Mateus Costa <mateus@ornito.com.br>
2018-08-08 19:29:03 -0300

    Criando modulo de squad

--- src/app/squads/Squads.vue
+++ src/app/squads/Squads.vue
@@ -1,13 +1,137 @@
 <template lang='pug'>
-
+.squads
+  Notification(
+    :title='notification.title',
+    :message='notification.message',
+    :type='notification.messageClass',
+    :open='notification.open',
+    :duration='3500'
+  )
+  .container
+    h1.title Squads
+    .columns
+      .column
+        router-link.button.is-squadDash.is-outlined(:to='{ path: `/squads/cadastro`}') Criar squade
+      .column
+        .control.has-icons-right
+          input#search.input.squadDash-input(type='text', placeholder='Pesquisar por squad', @input='searchTable()')
+          span.icon.is-right
+            i.fa.fa-search
+  .container
+    .table
+      table#table.table-content
+        thead
+          th.table-content-head-item Squad
+          th.table-content-head-item Funcionários
+          th.table-content-head-item Status
+          th.table-content-head-item
+        tbody
+          tr.table-content-row(v-for='(squad, index) in squads', v-if='!squad.deleted')
+            td.table-content-row-item {{squad.name}}
+            td.table-content-row-item
+              ListBadge(:items='squad.employees')
+            td.table-content-row-item {{squad.status | boolFormat('Ativado', 'Desativado')}}
+            td.table-content-row-item
+              .actions
+                router-link.actions-link(:to='{ path: `/squades/editar/${squad.id}` }')
+                  span.fa.fa-edit(title='editar projeto')
+                span.fa.fa-toggle-on.actions-link(v-if='squad.status', @click='changeStatus(false, index)', title='desativar projeto')
+                span.fa.fa-toggle-off.actions-link(v-if='!squad.status', @click='changeStatus(true, index)', title='ativar projeto')
+                span.fa.fa-trash.actions-link(title='excluir projeto', @click='openSquadDeleteModal(squad)')
+  //- squadDeleteModal(
+  //-   :squad='selectedsquad',
+  //-   :showModal='showsquadDeleteModal',
+  //-   @onClose='closesquadDeleteModal',
+  //-   @onSubmit='deletesquad'
+  //- )
 </template>
 
 <script>
+import _ from 'lodash'
+import Utils from '../../utils/index'
+import SquadDeleteModal from './SquadDeleteModal'
+import Notification from '../../components/Notification'
+import ListBadge from '../../components/ListBadge'
 export default {
-
+  components: {
+    SquadDeleteModal,
+    Notification,
+    ListBadge
+  },
+  computed: {
+    allSquads () {
+      return this.$store.state.Squads.allSquads
+    },
+    messageClass () {
+      return this.$store.state.Squads.messageClass
+    },
+    title () {
+      return this.$store.state.Squads.title
+    },
+    message () {
+      return this.$store.state.Squads.message
+    },
+    loading () {
+      return this.$store.state.loading
+    }
+  },
+  data () {
+    return {
+      squads: [],
+      showsquadDeleteModal: false,
+      selectedsquad: {},
+      notification: {
+        open: false
+      }
+    }
+  },
+  methods: {
+    changeStatus (value, index) {
+      let squad = this.squads[index]
+      squad.status = value
+      this.$store.dispatch('editsquad', squad).then(() => {
+        if (this.messageClass === 'success') {
+          this.openNotification(this.message, this.messageClass, this.title)
+        }
+      })
+    },
+    deletesquad (squad) {
+      squad.deleted = true
+      this.closesquadDeleteModal(false)
+      this.$store.dispatch('deletesquad', squad).then(() => {
+        if (this.messageClass === 'success') {
+          this.openNotification(this.message, this.messageClass, this.title)
+        }
+      })
+    },
+    openNotification (message, messageClass, title) {
+      this.notification.message = message
+      this.notification.messageClass = messageClass
+      this.notification.title = title
+      this.notification.open = true
+      setTimeout(() => { this.notification.open = false }, 3500)
+    },
+    openSquadDeleteModal (squad) {
+      this.selectedsquad = squad
+      this.showsquadDeleteModal = true
+    },
+    closesquadDeleteModal (value) {
+      this.showsquadDeleteModal = value
+    },
+    searchTable () {
+      Utils.searchTable()
+    }
+  },
+  created () {
+    this.$store.dispatch('getAllSquads').then(() => {
+      this.squads = _.clone(this.allSquads)
+    })
+  }
 }
 </script>
 
-<style>
-
+<style lang='scss'>
+.squads {
+  margin-top: 2%;
+}
 </style>
---
+++ src/components/ListBadge.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="list-badge">
+    <div class="list-badge-content" v-for="item in items" :key='item.name'>
+      <div class="list-badge-item">
+        <a @click='selectItem(item)'>{{item.name}}</a>
+        <a @click='deleteItem(item.id)'>X</a>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {type: Array, default: () => []}
+  },
+  methods: {
+    selectItem (item) {
+      this.$emit('onSelect', item)
+    },
+    deleteItem (id) {
+      this.$emit('onDelete', id)
+    }
+  }
+}
+</script>
+
+<style lang='scss'>
+.list-badge-item {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  background: #ccc;
+  padding: 0 5px;
+  margin-bottom: 5px;
+  a {
+    color: #00b89c;
+    &:hover {
+      font-weight: bold;
+    }
+  }
+}
+.list-badge-content {
+  border: 1px solid #00b89c;
+  border-radius: 10px;
+  height: 50px;
+  padding: 10px;
+  overflow-y: scroll;
+}
+</style>
